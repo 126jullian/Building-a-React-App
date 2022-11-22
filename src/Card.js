6bd82b543305851e1cbc98910ea5003b68b894a6
Jullian <jullian840126@gmail.com>
2022-11-23 00:06:14 +0800

    git commit -m "my first react component"

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div>
+            <img alt='robots' src='https://robohash.org/robot1?200×200' />
+            <div>
+                <h1>Jane Doe</h1>
+                <p>jane.doe@gmail.com</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;

1e9f5734f3328da0daebb56ebd0d4bdd65992e1e
Jullian <jullian840126@gmail.com>
2022-11-23 22:01:31 +0800

    git commit -m "Building A React App 2"

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({name, email, id}) => {
     return (
-        <div>
-            <img alt='robots' src='https://robohash.org/robot1?200×200' />
+        <div className='tc bg-light-green dib br3 bw2 shadow-5 pa3 ma2 grow'>
+            <img alt='robots' src={`https://robohash.org/${id}?200x200`} />
             <div>
-                <h1>Jane Doe</h1>
-                <p>jane.doe@gmail.com</p>
+                <h1>{name}</h1>
+                <p>{email}</p>
             </div>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Card from './Card';
+import CardList from './CardList'
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
+import { robots } from './robots';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Card/>
-    <Card/>
-    <Card/>
-  </React.StrictMode>
+            <React.StrictMode>
+            <CardList robots={robots}/>
+            </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
